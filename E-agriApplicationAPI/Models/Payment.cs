67342e8fb21645369df01c35057356aa95b748a0
koumiya <koumiya49>
2023-09-05 16:08:40 +0530

    creating a database connection

---
+++ E-agriApplicationAPI/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_agriApplicationAPI.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CartId { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ E-agriApplicationAPI/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_agriApplicationAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Ids { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int UserId { get; set; }
+        //navigation property
+        public Users users { get ; set; }
+       
+
+
+    }
+}
